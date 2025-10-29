6b5304cfeccfb3abe61b05074e2e66d34b7f09ea
LAB23_USER ON WS21 <L23X16W21@LabHCI.net>
2025-10-29 17:35:07 +0800

    no https

--- bustrackr_system/bustrackr_system/settings.py
+++ bustrackr_system/bustrackr_system/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://bustrackrcebu.onrender.com"]
+ALLOWED_HOSTS = ["bustrackrcebu.onrender.com"]
 
 
 # Application definition
